7034b7312b9e355ea3833e2cbc279fd70ca0f72a
MemeFrug <knightmax22@gmail.com>
2022-02-08 16:03:19 +0930

    Update main.js

--- docs/Games/ICTProjects/Javascript Draw/JS/main.js
+++ docs/Games/ICTProjects/Javascript Draw/JS/main.js
@@ -68,6 +68,20 @@ Resize()
 Draw()
 
 // Phone
+function copyTouch({ identifier, pageX, pageY }) {
+    return { identifier, pageX, pageY };
+  }
+
+  function ongoingTouchIndexById(idToFind) {
+    for (var i = 0; i < ongoingTouches.length; i++) {
+      var id = ongoingTouches[i].identifier;
+  
+      if (id == idToFind) {
+        return i;
+      }
+    }
+    return -1;    // not found
+  }
 
 function startup() {
     var el = document.getElementById("canvas");
@@ -91,10 +105,8 @@ function startup() {
     for (var i = 0; i < touches.length; i++) {
       console.log("touchstart:" + i + "...");
       ongoingTouches.push(copyTouch(touches[i]));
-      var color = colorForTouch(touches[i]);
       ctx.beginPath();
-      ctx.arc(touches[i].pageX, touches[i].pageY, 4, 0, 2 * Math.PI, false);  // a circle at the start
-      ctx.fillStyle = color;
+      ctx.strokeStyle = colorpick.value;
       ctx.fill();
       console.log("touchstart:" + i + ".");
     }
@@ -107,7 +119,6 @@ function startup() {
     var touches = evt.changedTouches;
   
     for (var i = 0; i < touches.length; i++) {
-      var color = colorForTouch(touches[i]);
       var idx = ongoingTouchIndexById(touches[i].identifier);
   
       if (idx >= 0) {
@@ -118,7 +129,7 @@ function startup() {
         console.log("ctx.lineTo(" + touches[i].pageX + ", " + touches[i].pageY + ");");
         ctx.lineTo(touches[i].pageX, touches[i].pageY);
         ctx.lineWidth = 4;
-        ctx.strokeStyle = color;
+        ctx.strokeStyle = colorpick.value;
         ctx.stroke();
   
         ongoingTouches.splice(idx, 1, copyTouch(touches[i]));  // swap in the new touch record
@@ -131,22 +142,19 @@ function startup() {
 
   function handleEnd(evt) {
     evt.preventDefault();
-    log("touchend");
     var el = document.getElementById("canvas");
     var ctx = el.getContext("2d");
     var touches = evt.changedTouches;
   
     for (var i = 0; i < touches.length; i++) {
-      var color = colorForTouch(touches[i]);
       var idx = ongoingTouchIndexById(touches[i].identifier);
   
       if (idx >= 0) {
         ctx.lineWidth = 4;
-        ctx.fillStyle = color;
+        ctx.fillStyle = colorpick.value;;
         ctx.beginPath();
         ctx.moveTo(ongoingTouches[idx].pageX, ongoingTouches[idx].pageY);
         ctx.lineTo(touches[i].pageX, touches[i].pageY);
-        ctx.fillRect(touches[i].pageX - 4, touches[i].pageY - 4, 8, 8);  // and a square at the end
         ongoingTouches.splice(idx, 1);  // remove it; we're done
       } else {
         console.log("can't figure out which touch to end");
