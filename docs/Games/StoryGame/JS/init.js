0a52c7850b8a5709e722ea00dc0daa8bcb744931
MemeFrug <knightmax22@gmail.com>
2022-02-17 16:03:26 +0930

    Update init.js

--- docs/Games/StoryGame/JS/init.js
+++ docs/Games/StoryGame/JS/init.js
@@ -56,6 +56,23 @@ window.onload = async () => {
 async function Play() {
     await animate((time) => { return time }, Animations.opacity_buttons_none, 2000, 1) // Play An Animation, but wait till it it resoved by the Promise
     MainMenuElementDOM.style.display = "none" // Remove The Buttons Completely so they dont get into the way
+    document.getElementById("Settings-Button").removeEventListener("mouseup", Settings)
+    document.getElementById("Back-Button").removeEventListener("mouseup", BackToHub)
+    document.getElementById("Play-Button").removeEventListener("mouseup", Play)
+}
+
+/**
+ * Opens up the settings menu
+ */
+async function Settings() {
+
+}
+
+/**
+ * Changes The Page To The Main Hub Of the Webstie
+ */
+async function BackToHub() {
+    window.location.replace("../../index.html"); // Send The Player Back to The Hub
 }
 
 /**
@@ -71,10 +88,6 @@ window.onfocus = () => {
 /**
  * Detect When A Button Is Pressed
  */
-document.getElementById("Back-Button").addEventListener("mouseup", () => {
-    window.location.replace("../../index.html"); // Send The Player Back to The Hub
-})
-document.getElementById("Settings-Button").addEventListener("mouseup", () => { // When The Settings Button Is Clicked
-
-})
+document.getElementById("Back-Button").addEventListener("mouseup", BackToHub) // When the Back to hub button is clicked
+document.getElementById("Settings-Button").addEventListener("mouseup", Settings) // When the settings button is clicked
 document.getElementById("Play-Button").addEventListener("mouseup", Play) // Start The Play Function
