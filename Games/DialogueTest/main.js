1ef49ff474c6799448f6e32e608adf3b8e35fd5f
MemeFrug <knightmax22@gmail.com>
2022-04-06 02:07:16 +0930

    Added Music To Sans Dialogue

--- Games/DialogueTest/main.js
+++ Games/DialogueTest/main.js
@@ -1,9 +1,30 @@
 const loadingScreen = createLoadingScreenFromDOM(document.getElementById("LoadingElement"), "flex")
 const SideLaugh = document.createElement("img")
 const Default = document.createElement("img")
-const TextToSay = "Woah, whatcha doing there son???"
+const TextToSay = "Woah, arnt u a sussy baka!!!"
 let ImageShowing = Default
 let TextShowing = "* "
+
+async function ReadText(Text = TextToSay) {
+    TextShowing = "* "
+    ImageShowing = Default
+    for (let i = 0; i < Text.length; i++) {
+        const element = Text[i];
+        new Audio("./Assets/snd_txtsans.wav").play()
+        TextShowing += element
+        if (element == ",") {
+            await sleep(500)
+            ImageShowing = SideLaugh
+        } else if (element == "?") {
+            await sleep(700)
+        }else {
+            await sleep(80)
+        }
+    }
+    await sleep(500)
+    ImageShowing = Default
+    return
+}
 
 function setup() {
     createCanvas(true)
@@ -13,19 +34,7 @@ function setup() {
 
 async function onload() {
     loadingScreen.destroy()
-    
-    for (let i = 0; i < TextToSay.length; i++) {
-        const element = TextToSay[i];
-        TextShowing += element
-        if (element == ",") {
-            await sleep(500)
-            ImageShowing = SideLaugh
-        } else if (element == "?") {
-            await sleep(700)
-        }else {
-            await sleep(50)
-        }
-    }
+    ReadText()
 }
 
 function draw(ctx) {
@@ -36,6 +45,6 @@ function draw(ctx) {
     ctx.strokeRect(ENGINE.Config.nativeWidth / 4, ENGINE.Config.nativeHeight / 1.6, ENGINE.Config.nativeWidth / 2, 300)
     ctx.fillStyle = "rgb(216, 216, 216)"
     ctx.font = "40px DTM-Sans"
-    ctx.fillText(TextShowing, 750, 800)
+    ctx.fillText(TextShowing, 760, 800)
     ctx.drawImage(ImageShowing, 500, 700, 250, 250)
 }
