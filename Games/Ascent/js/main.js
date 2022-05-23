dde56a446f40974e197ce1b5eb9ffed4b8390a1f
MemeFrug <knightmax22@gmail.com>
2022-05-23 09:37:39 +0930

    DId text, dialogue in game

--- Games/Ascent/js/main.js
+++ Games/Ascent/js/main.js
@@ -1,5 +1,6 @@
 let currentLevelIndex = 0
 let showingYouDieScreen = false
+let textShowing = ""
 
 function sound(src) {
     this.sound = document.createElement("audio");
@@ -14,13 +15,18 @@ function sound(src) {
     this.stop = function(){
       this.sound.pause();
     }
+    this.loop = function(){
+        this.sound.loop = !this.sound.loop
+    }
   }
 
 const music = new sound("./assets/background1.mp3")
+music.loop()
 
 function setup() {
     const player = new Player(false, 0, 10, 90, 190)
-    player.setImg("./assets/evil nuthead.gif", false);
+    player.c = "red"
+    // player.setImg("./assets/evil nuthead.gif", false);
 
     // player.gravityMax = -1000
     player.jump_strength = -860
@@ -32,6 +38,8 @@ function setup() {
     ENGINE.Config.cameraScale = 1
     ENGINE.addPlayer(player, true)
 
+    // addEngineEvent(Enum.Events.Pressed.)
+
     WORLD.blockSize = 100
 
     STATS.new().autoLoad()
@@ -42,11 +50,40 @@ function setup() {
         await loadLevel(Levels[0])
         getElementById("LoadingScreen").style.display = "none"
         createCanvas(true)
-        music.play()
         setCanvasBackground("white")
+        music.play()
+
+        await showText("I love giving head to all men across the world, worldwide, and in my basement.")
+        await showText("Woah, WHere am i?")
     }
 }
 
+function showText(text) {
+    return new Promise(async (resolve, reject) => {
+        textShowing = ""
+        for (let i = 0; i < text.length; i++) {
+            const element = text[i];
+            // const SansAudio = new Audio("./Assets/snd_txtsans.wav")
+            // SansAudio.volume = 0.3
+            // SansAudio.play()
+            textShowing += element
+            if (element == ",") {
+                await sleep(500)
+            } else if (element == "?") {
+                await sleep(700)
+            } else if (element == "!") {
+                await sleep(300)
+            } else if (element == ".") {
+                await sleep(700)
+            }else {
+                await sleep(40)
+            }
+        }
+        resolve()
+    });
+}
+
+//TODO Add event listeners uwu
 function update(deltaTime) {
     const player = ENGINE.GetLocalPlayer()
     if (ENGINE.InputHandler.keys_down.w) player.move('w'); // Move the player
@@ -56,7 +93,14 @@ function update(deltaTime) {
 	if (ENGINE.InputHandler.keys_down.d) player.move('d');
 	else player.stopMove("d")
 
-    if (player.y > Levels[currentLevelIndex].length * WORLD.blockSize - 1500) {
+    // if (ENGINE.InputHandler.keys_down.W) player.move('w'); // Move the player
+	// else player.stopMove("w")
+	// if (ENGINE.InputHandler.keys_down.A) player.move('a');
+	// else player.stopMove("a")
+	// if (ENGINE.InputHandler.keys_down.D) player.move('d');
+	// else player.stopMove("d")
+
+    if (player.y > Levels[currentLevelIndex].length * WORLD.blockSize - 3000) {
         console.log("u die");
         showingYouDieScreen = true
     }
@@ -74,14 +118,24 @@ function afterDraw(ctx) {
 }
 
 function drawUI(ctx) {
-
     if (showingYouDieScreen) {
         fillRect(0, 0, ENGINE.Config.nativeWidth, ENGINE.Config.nativeHeight, "black")
         ctx.fillStyle = "red"
         ctx.font = "100px Arial";
         fillText("u dye", ENGINE.Config.nativeWidth / 2 - 150, ENGINE.Config.nativeHeight / 2)
-
     }
+
+
+    ctx.globalAlpha = 0.8
+    ctx.fillStyle = "rgba(0, 0, 0, 1)"
+    ctx.fillRect(ENGINE.Config.nativeWidth / 4, ENGINE.Config.nativeHeight / 1.3, ENGINE.Config.nativeWidth / 2, 200)
+    // ctx.strokeStyle = "white"
+    // ctx.lineWidth = 9
+    // ctx.strokeRect(ENGINE.Config.nativeWidth / 4, ENGINE.Config.nativeHeight / 1.6, ENGINE.Config.nativeWidth / 2, 300)
+    ctx.fillStyle = "rgb(216, 216, 216)"
+    ctx.font = "40px DTM-Sans"
+    fillText("David: ", 550, ENGINE.Config.nativeHeight / 1.2)
+    fillText(textShowing, 760, ENGINE.Config.nativeHeight / 1.2, 650, 40)
 }
 
 function deleteLevel() {
@@ -108,10 +162,10 @@ function loadLevel(levelData) {
             {name: "background", asset: "./Assets/backgroundBrick.png", dataValue: 1, noCollision: true},
             {name: "brick", asset: "./Assets/brick.png", dataValue: 2},
             {name: "spike1", asset: "./Assets/spike1.png", dataValue: 3, noCollision: true},
-            {name: "stairs", asset: "./Assets/stairs.png", dataValue: 4},
-            {name: "stairsReversed", asset: "./Assets/stairsReversed.png", dataValue: 5},
-            {name: "stairsUpsidedown", asset: "./Assets/stairsUpsidedown.png", dataValue: 6},
-            {name: "stairsUpsidedownReversed", asset: "./Assets/stairsUpsidedownReversed.png", dataValue: 7},
+            {name: "stairs", asset: "./Assets/stairs.png", dataValue: 4, noCollision: true},
+            {name: "stairsReversed", asset: "./Assets/stairsReversed.png", dataValue: 5, noCollision: true},
+            {name: "stairsUpsidedown", asset: "./Assets/stairsUpsidedown.png", dataValue: 6, noCollision: true},
+            {name: "stairsUpsidedownReversed", asset: "./Assets/stairsUpsidedownReversed.png", dataValue: 7, noCollision: true},
         ]
     
         for (let i = 0; i < key.length; i++) {
